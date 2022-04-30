edefd570b5d0be4f4f600301f6c950a863004748
Knut Hühne <knut@k-nut.eu>
2022-04-30 15:36:09 +0200

    Add button to confirm new bet creation

--- src/ShareOverview/Bet.js
+++ src/ShareOverview/Bet.js
@@ -11,7 +11,7 @@ class Bet extends Component {
       start_date: props.bet.start_date,
       end_date: props.bet.end_date,
       value: props.bet.value,
-      id: props.bet.id
+      id: props.bet.id,
     };
     this.changeStart = this.changeStart.bind(this);
     this.changeEnd = this.changeEnd.bind(this);
@@ -25,10 +25,6 @@ class Bet extends Component {
     );
   }
 
-  update = debounce(() => {
-    this.props.updateCallback(this.state);
-  }, 500);
-
   changeEnd(end_date) {
     this.setState({ end_date: end_date.format().slice(0, 10) }, this.update);
   }
@@ -38,22 +34,16 @@ class Bet extends Component {
   }
 
   render() {
-    const months = range(72).map(i => {
-      return moment
-        .utc("2017-01-01")
-        .startOf("day")
-        .add(i, "months");
+    const months = range(72).map((i) => {
+      return moment.utc("2017-01-01").startOf("day").add(i, "months");
     });
 
-    const halfMonths = range(72).map(i => {
-      return moment
-        .utc("2017-01-15")
-        .startOf("day")
-        .add(i, "months");
+    const halfMonths = range(72).map((i) => {
+      return moment.utc("2017-01-15").startOf("day").add(i, "months");
     });
     const startDates = [...months, ...halfMonths];
 
-    const endMonths = range(72).map(i => {
+    const endMonths = range(72).map((i) => {
       return moment
         .utc("2017-01-01")
         .add(i, "months")
@@ -90,10 +80,16 @@ class Bet extends Component {
           Betrag:
           <Input type="number" value={value} onChange={this.changeValue} />
         </label>
-        {this.props.bet.id && (
+        {this.props.bet.id ? (
           <Button
             onClick={() => this.props.deleteCallback(this.props.bet.id)}
             content="Löschen"
+          />
+        ) : (
+          <Button
+            disabled={!(this.state.start_date && this.state.value)}
+            onClick={() => this.props.updateCallback(this.state)}
+            content="Hinzufügen"
           />
         )}
       </div>
