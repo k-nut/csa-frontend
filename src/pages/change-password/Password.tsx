5ec4c1171d2dd54bca46449ae62fc9bfa116b83e
Knut Hühne <knut@k-nut.eu>
2022-12-09 20:16:44 +0100

    Redirect user after required password change

--- src/pages/change-password/Password.tsx
+++ src/pages/change-password/Password.tsx
@@ -4,8 +4,9 @@ import { Form } from "semantic-ui-react";
 import Api from "../../services/Api";
 import toast from "../../components/Toast";
 import styled from "styled-components";
-import { useLocation } from "react-router";
+import { useHistory, useLocation } from "react-router";
 import axios from "axios";
+import authState from "../../services/AuthState";
 
 const Container = styled.div`
   padding: 20px;
@@ -15,24 +16,30 @@ const PasswordChange: FunctionComponent = () => {
   const [password, setPassword] = useState("");
   const [passwordRepeat, setPasswordRepeat] = useState("");
   const { search } = useLocation();
+  const history = useHistory();
   const urlParams = new URLSearchParams(search);
 
-  const changePassword = () => {
-    Api.changePassword(password)
-      .then(() => toast.success("Passwort geändert"))
-      .catch((e) => {
-        if (axios.isAxiosError(e)) {
-          const error = e.response?.data?.validation_error?.body_params?.[0];
-          if (error.type === "value_error.any_str.min_length") {
-            toast.error(`Das Passwort muss mindestens ${error?.ctx?.limit_value} Zeichen lang sein.
+  const changePassword = async () => {
+    try {
+      await Api.changePassword(password);
+      toast.success("Passwort geändert");
+      if (urlParams.has("mustChange")) {
+        authState.needsPasswordChange = false;
+        history.push("/");
+      }
+    } catch (e) {
+      if (axios.isAxiosError(e)) {
+        const error = e.response?.data?.validation_error?.body_params?.[0];
+        if (error.type === "value_error.any_str.min_length") {
+          toast.error(`Das Passwort muss mindestens ${error?.ctx?.limit_value} Zeichen lang sein.
                    Bitte ändere es und probiere es erneut.`);
-          } else {
-            throw new Error("unknown backend error");
-          }
         } else {
-          toast.error("Ein Fehler ist aufgetreten");
+          throw new Error("unknown backend error");
         }
-      });
+      } else {
+        toast.error("Ein Fehler ist aufgetreten");
+      }
+    }
   };
 
   const mustChangeMessage =
