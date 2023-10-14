50a9cbe93acf0fe4191037236388cb8660db812c
Knut Hühne <knut@k-nut.eu>
2023-10-14 16:24:07 +0200

    Add color highliting to security cells

--- src/pages/payment-status-list/Share.tsx
+++ src/pages/payment-status-list/Share.tsx
@@ -6,8 +6,12 @@ import { ShareModel } from "../../services/Api";
 interface ShareProps {
   share: ShareModel;
 }
+const EXPECTED_SECURITY = 120;
+
 export const Share: FunctionComponent<ShareProps> = ({ share }) => {
   const state = share.difference_today < 0 ? "negative" : "positive";
+  const securityState =
+    share.total_security === EXPECTED_SECURITY ? "positive" : "negative";
   return (
     <Table.Row className="share" warning={share.archived}>
       <Table.Cell>{share.id}</Table.Cell>
@@ -20,7 +24,7 @@ export const Share: FunctionComponent<ShareProps> = ({ share }) => {
         <div style={{ whiteSpace: "pre-wrap" }}>{share.note}</div>
       </Table.Cell>
       <Table.Cell> {share.station_name} </Table.Cell>
-      <Table.Cell> {share.total_security} </Table.Cell>
+      <Table.Cell className={securityState}>{share.total_security} </Table.Cell>
       <Table.Cell> {share.number_of_deposits}</Table.Cell>
       <Table.Cell className={state}>{share.difference_today}</Table.Cell>
     </Table.Row>
