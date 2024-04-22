66b1fd23040db17bc6de9f34c8abfa313648044b
Knut Hühne <knut@k-nut.eu>
2024-04-22 18:12:53 +0200

    Highlight positive differences orange

--- src/pages/payment-status-list/Share.tsx
+++ src/pages/payment-status-list/Share.tsx
@@ -8,8 +8,17 @@ interface ShareProps {
 }
 const EXPECTED_SECURITY = 120;
 
+const state = (difference: number): "negative" | "positive" | "warning" => {
+  if (difference < 0) {
+    return "negative";
+  }
+  if (difference > 0) {
+    return "warning";
+  }
+  return "positive";
+};
+
 export const Share: FunctionComponent<ShareProps> = ({ share }) => {
-  const state = share.difference_today < 0 ? "negative" : "positive";
   const securityState =
     share.total_security === EXPECTED_SECURITY ? "positive" : "negative";
   return (
@@ -26,7 +35,9 @@ export const Share: FunctionComponent<ShareProps> = ({ share }) => {
       <Table.Cell> {share.station_name} </Table.Cell>
       <Table.Cell className={securityState}>{share.total_security} </Table.Cell>
       <Table.Cell> {share.number_of_deposits}</Table.Cell>
-      <Table.Cell className={state}>{share.difference_today}</Table.Cell>
+      <Table.Cell className={state(share.difference_today)}>
+        {share.difference_today}
+      </Table.Cell>
     </Table.Row>
   );
 };
