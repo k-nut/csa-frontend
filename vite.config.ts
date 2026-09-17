62156879551ea388150316bf160ac010e76630fe
Knut Hühne <knut@k-nut.eu>
2026-09-17 17:01:04 +0200

    Ensure js assets are correctly loaded when on nested sub-urls

--- vite.config.ts
+++ vite.config.ts
@@ -137,7 +137,7 @@ function basePlugin(): Plugin {
     config(_, { mode }) {
       const { PUBLIC_URL } = loadEnv(mode, ".", ["PUBLIC_URL"]);
       return {
-        base: PUBLIC_URL || "",
+        base: PUBLIC_URL || "/",
       };
     },
   };
